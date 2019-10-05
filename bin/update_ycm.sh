d75c85d57fe5ec38ad42db922f82b03c2df75573
ct <cemtunaboylu9@gmail.com>
2019-10-05 17:42:12 +0300

    YCM code fixed, path to python changed. anaconda3 is not compatible with ycm.

--- bin/update_ycm.sh
+++ bin/update_ycm.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo Updating YCM...
-
 cd ~/.vim/bundle/YouCompleteMe
 echo Checking if Mono exists or not ...
 # if not brew install mono ; brew link --overwrite --dry-run mono if yes: brew link --overwrite mono
@@ -13,18 +12,18 @@ curl -O https://download.oracle.com/otn/java/jdk/8u221-b11/230deb18db3e4014bb8e3
 echo Downloaded the JDK in Home directory
 
 #echo Compiling YCM with semantic support for C-family languages, C#, JavaScript, Java
-#./install.py --clangd-completer --cs-completer --ts-completer --java-completer
+~/../../usr/bin/python install.py --clangd-completer --cs-completer --ts-completer --java-completer
 
-cd ~ 
-mkdir ycm_build
-cd ycm_build
+#cd ~ 
+#mkdir ycm_build
+#cd ycm_build
 
-cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+#cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 
-cmake --build . --target ycm_core --config Release
+#cmake --build . --target ycm_core --config Release
 
 # C# handling
-cd ..
-cd YouCompleteMe/third_party/ycmd/third_party/omnisharp-roslyn
-curl -OL https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.34.4/omnisharp-osx.tar.gz
-tar -xzvf omnisharp-osx.tar.gz
+#cd ..
+#cd YouCompleteMe/third_party/ycmd/third_party/omnisharp-roslyn
+#curl -OL https://github.com/OmniSharp/omnisharp-roslyn/releases/download/v1.34.4/omnisharp-osx.tar.gz
+#tar -xzvf omnisharp-osx.tar.gz
